b0f7469b5a6e64ec2c1b68f7da36acd447f51039
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-07 17:05:53 +0100

    seleziono il giusto elemento sul qula cliccare e lo rendo dunque display:block così da poterlo cliccare; Al click poi,tramite addeventlistner, seleziono il div .hamburger-menu e rendo quest'ultimo a sua volta di tipo block così da poterlo visualizzare

--- main.js
+++ main.js
@@ -1,6 +1,13 @@
-const element = document.querySelector('.hamburger-menu');
+console.log('JS OK');
+const element = document.querySelector('.header-right > a');
+element.style.display = "block";
 element.addEventListener('click',function(){
-    element.style.display = "block";
+    const div = document.querySelector('.hamburger-menu');
+    div.style.display = "block";
 }
-
 );
+
+
+
+
+
