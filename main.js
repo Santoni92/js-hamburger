7f108c7f02287a330c619714401042ce902c3ed5
Santoni92 <98034624+Santoni92@users.noreply.github.com>
2022-03-07 17:16:05 +0100

    seleziono il tag 'a'  figlio diretto del div .hamburger-menu e ci aggiungo l'evento al click così da rendere questo div di tipo none (cioè chiuderlo al click)

--- main.js
+++ main.js
@@ -1,11 +1,17 @@
 console.log('JS OK');
 const element = document.querySelector('.header-right > a');
 element.style.display = "block";
+const div = document.querySelector('.hamburger-menu');
 element.addEventListener('click',function(){
-    const div = document.querySelector('.hamburger-menu');
+    
     div.style.display = "block";
 }
 );
+
+const a = document.querySelector('.hamburger-menu > a');
+a.addEventListener('click',function(){
+    div.style.display = "none";
+});
 
 
 
